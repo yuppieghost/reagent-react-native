50ae1df5d98f1bf3efdb62aaeebb13419b1e37ad
ouzm <wjj315315@gmail.com>
2022-01-23 23:51:28 +0800

    add: re-frame subs

---
+++ AwesomeProject/app/dev.subs.js
@@ -0,0 +1,6 @@
+goog.provide('dev.subs');
+re_frame.core.reg_sub.cljs$core$IFn$_invoke$arity$variadic(new cljs.core.Keyword("dev.subs","name","dev.subs/name",-1497279323),cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2([(function (db){
+return new cljs.core.Keyword(null,"name","name",1843675177).cljs$core$IFn$_invoke$arity$1(db);
+})], 0));
+
+//# sourceMappingURL=dev.subs.js.map
--- src/main/dev/views.cljs
+++ src/main/dev/views.cljs
@@ -1,12 +1,25 @@
 (ns dev.views
   (:require
+   ["react-native" :as rn]
    [re-frame.core :as re-frame]
    [dev.subs :as subs]
    ))
 
-(defn main-panel []
+
+(def styles
+  ^js (-> {:container
+           {:flex 1
+            :backgroundColor "#fff"
+            :alignItems "center"
+            :justifyContent "center"}
+           :title
+           {:fontWeight "bold"
+            :fontSize 24
+            :color "blue"}}
+          (clj->js)
+          (rn/StyleSheet.create)))
+
+(defn root []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Hello from " @name]
-     ]))
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/Text {:style (.-title styles)} "Hello !" @name]]))
